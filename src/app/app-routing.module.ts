b7afac249770603b1a9b26e758bb1e3130370e17
Etosticity <etosticity@gmail.com>
2017-11-28 10:46:40 +0800

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { TeamComponent } from './team/team.component';
+import { MembersComponent } from './members/members.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'team', component: TeamComponent },
+  { path: 'members', component: MembersComponent },
+  { path: 'aboutus', component: AboutUsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { MaterializeModule } from 'angular2-materialize';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './footer/footer.component';
+import { TeamComponent } from './team/team.component';
+import { MembersComponent } from './members/members.component';
+import { AboutUsComponent } from './about-us/about-us.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    FooterComponent,
+    TeamComponent,
+    MembersComponent,
+    AboutUsComponent
   ],
   imports: [
     BrowserModule,
