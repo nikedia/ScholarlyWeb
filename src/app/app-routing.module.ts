13060ccf8d27a179c832893483fa5d6c9b062b01
Etosticity <etosticity@gmail.com>
2017-12-14 23:50:53 +0800

    Redone navbar to proposed design, rebuilt live beta website

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { TeamComponent } from './team/team.component';
-import { MembersComponent } from './members/members.component';
+// import { TeamComponent } from './team/team.component';
+// import { MembersComponent } from './members/members.component';
 import { AboutUsComponent } from './about-us/about-us.component';
-import { ContactUsComponent } from './contact-us/contact-us.component';
+// import { ContactUsComponent } from './contact-us/contact-us.component';
 import { DashboardComponent } from './users/dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'team', component: TeamComponent },
-  { path: 'members', component: MembersComponent },
+  // { path: 'team', component: TeamComponent },
+  // { path: 'members', component: MembersComponent },
   { path: 'aboutus', component: AboutUsComponent },
-  { path: 'contactus', component: ContactUsComponent },
+  // { path: 'contactus', component: ContactUsComponent },
   { path: 'dashboard', component: DashboardComponent }
 ];
 
